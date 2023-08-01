9acad9b209d875f867668a13f108b22fafaf01bf
AmirAli-Azimi <amiraliazimi2006@gmail.com>
2023-08-01 14:56:45 +0330

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int[] list=new int[3];
+for (int i = 0; i<list.Length;i++)
+{
+    list[i]=int.Parse(Console.ReadLine());
+}
+System.Console.WriteLine("print array list....");
+for(int e=0; e<list.Length;e++)
+{
+    System.Console.WriteLine(list[e]);
+}
---
+++ productabs.cs
@@ -0,0 +1,16 @@
+abstract class pro
+{
+
+public int id ;
+public int date;
+public string name;
+public decimal mark;
+void print_mark()
+{
+    System.Console.WriteLine(name);
+}
+}
+class str:pro
+{
+
+}
